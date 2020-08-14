791a55748028591ccc6d3599b05fbe6910a182bf
mariaterezaccf <senhora.mariatereza@gmail.com>
2020-08-14 09:43:52 -0300

    Idade.java

---
+++ Data/Idade.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Idade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vamos cadastrar sua data de nascimento! Digite um dia do mês.");
+        int diaMes = scanner.nextInt();
+
+        System.out.println("Digite um mês.");
+        int mesAno = scanner.nextInt();
+
+        System.out.println("Digite um ano.");
+        int ano = scanner.nextInt();
+
+
+
+    }
+}
